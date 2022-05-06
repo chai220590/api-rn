97b709067c826c64cf6fb163f1d077029eeabc95
Thao. Quan Ngoc - CMC Global DU1.19 <qnthao@cmcglobal.vn>
2022-05-06 13:14:14 +0700

    add fixed helmet

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
-import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  app.enableCors();
-  const config = new DocumentBuilder()
-    .addBearerAuth()
-    .setTitle('Learning React Native API')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
+import { AppModule } from './app.module';
+function disableUpgradeInsecureRequests(app: any, helmet: any) {
   const defaultDirectives = helmet.contentSecurityPolicy.getDefaultDirectives();
   delete defaultDirectives['upgrade-insecure-requests'];
 
@@ -23,6 +14,19 @@ async function bootstrap() {
       },
     }),
   );
+}
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Learning React Native API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+  app.use(helmet());
+  disableUpgradeInsecureRequests(app, helmet);
   await app.listen(process.env.PORT || 22220);
 }
 bootstrap();
