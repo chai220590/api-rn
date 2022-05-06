5f6cfbd7866bd8eed523fd6dbcc4357e158c9b8c
Thao. Quan Ngoc - CMC Global DU1.19 <qnthao@cmcglobal.vn>
2022-05-06 11:17:35 +0700

    fix https

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,14 +9,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { AppGateway } from './app.gateway';
 import { MessageModule } from './message/message.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client'),
-    }),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.SITE_CONNECTION_STRING),
     UserModule,
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
@@ -12,7 +13,16 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
-  // app.use(helmet());
+  const defaultDirectives = helmet.contentSecurityPolicy.getDefaultDirectives();
+  delete defaultDirectives['upgrade-insecure-requests'];
+
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        ...defaultDirectives,
+      },
+    }),
+  );
   await app.listen(process.env.PORT || 22220);
 }
 bootstrap();
